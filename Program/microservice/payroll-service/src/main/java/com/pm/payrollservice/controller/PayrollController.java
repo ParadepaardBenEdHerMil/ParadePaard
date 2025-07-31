1417df88ff134945a5b022e0e3fd54214d9f6537
Moodhood <Bevanrhee@gmail.com>
2025-07-31 17:49:46 +0200

    setup for payroll service (entry point, and db + schema)

---
+++ Program/microservice/payroll-service/src/main/java/com/pm/payrollservice/controller/PayrollController.java
@@ -0,0 +1,24 @@
+package com.pm.payrollservice.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payroll")
+@Tag(name = "Payroll", description = "API for managing payroll service")
+public class PayrollController {
+
+    private static final Logger log = LoggerFactory.getLogger(PayrollController.class);
+
+    @GetMapping
+    @Operation(summary = "Get Payslips test")
+    public String getPayslips() {
+        log.info("hit getPayslips endpoint");
+        return "logged";
+    }
+}
