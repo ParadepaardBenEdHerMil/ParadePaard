861238fbca87564dbf60e237e4d2bce712e09249
Moodhood <Bevanrhee@gmail.com>
2025-08-03 23:28:55 +0200

    Timesheet-service for logged hour retrieval

---
+++ Program/microservice/Timesheet-service/src/test/java/com/pm/timesheetservice/TimesheetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.timesheetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimesheetServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
